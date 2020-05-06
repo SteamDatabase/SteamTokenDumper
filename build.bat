fda78a64f0fa88c6d5e43ebad562c13ccf50d215
Pavel Djundik <xPaw@users.noreply.github.com>
2020-05-06 17:07:15 +0300

    Move PublishSingleFile to script

--- build.bat
+++ build.bat
@@ -2,12 +2,12 @@
 
 :: WINDOWS
 rmdir /Q /S bin\Release
-dotnet publish -c Release --runtime win-x64
+dotnet publish -c Release -p:PublishSingleFile=true --runtime win-x64
 bash -c "zip -9j SteamTokenDumper.zip bin/Release/win-x64/publish/SteamTokenDumper.exe"
 
 :: LINUX
 rmdir /Q /S bin\Release
-dotnet publish -c Release --runtime linux-x64
+dotnet publish -c Release -p:PublishSingleFile=true --runtime linux-x64
 bash -c "env GZIP=-9 tar cvzf SteamTokenDumper-linux.tar.gz -C bin/Release/linux-x64/publish/ SteamTokenDumper"
 
 rmdir /Q /S bin\Release
