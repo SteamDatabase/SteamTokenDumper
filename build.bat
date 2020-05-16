41b4ae8ca7f61b4822739d75c11f123653443e5f
Pavel Djundik <xPaw@users.noreply.github.com>
2020-05-16 21:18:30 +0300

    Delete obj folder when building

--- build.bat
+++ build.bat
@@ -1,13 +1,16 @@
 @echo off
 
 :: WINDOWS
+rmdir /Q /S obj
 rmdir /Q /S bin\Release
 dotnet publish -c Release -p:PublishSingleFile=true --runtime win-x64
 bash -c "zip -9j SteamTokenDumper.zip bin/Release/win-x64/publish/SteamTokenDumper.exe"
 
 :: LINUX
+rmdir /Q /S obj
 rmdir /Q /S bin\Release
 dotnet publish -c Release -p:PublishSingleFile=true --runtime linux-x64
 bash -c "env GZIP=-9 tar cvzf SteamTokenDumper-linux.tar.gz --owner=0 --group=0 -C bin/Release/linux-x64/publish/ SteamTokenDumper"
 
+rmdir /Q /S obj
 rmdir /Q /S bin\Release
