8540bedf364ce9bedba9d953b5ee26c541297ecc
Pavel Djundik <xPaw@users.noreply.github.com>
2020-09-19 13:12:24 +0300

    Target net5

--- build.bat
+++ build.bat
@@ -1,22 +1,40 @@
 @echo off
 
+echo.
+echo :: PREPARING
+echo.
+
+SET DOTNET_CLI_TELEMETRY_OPTOUT=1
+
 set /p BuildToken=<apiclient_token.txt
 move ApiClient.cs ApiClient_build.cs.tmp
 powershell -Command "(gc ApiClient_build.cs.tmp) -replace '@STEAMDB_BUILD_TOKEN@', '%BuildToken%' | Out-File -encoding ASCII ApiClient.cs"
+
+echo.
+echo :: BUILDING WINDOWS
+echo.
 
 :: WINDOWS
 del SteamTokenDumper.zip
 rmdir /Q /S obj
 rmdir /Q /S bin\Release
-dotnet publish -c Release --runtime win-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesInSingleFile=true
+dotnet publish -c Release --runtime win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
 bash -c "zip -9j SteamTokenDumper.zip bin/Release/win-x64/publish/SteamTokenDumper.exe"
+
+echo.
+echo :: BUILDING LINUX
+echo.
 
 :: LINUX
 del SteamTokenDumper-linux.tar.gz
 rmdir /Q /S obj
 rmdir /Q /S bin\Release
-dotnet publish -c Release -p:PublishSingleFile=true --runtime linux-x64
+dotnet publish -c Release --runtime linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
 bash -c "env GZIP=-9 tar cvzf SteamTokenDumper-linux.tar.gz --owner=0 --group=0 -C bin/Release/linux-x64/publish/ SteamTokenDumper"
+
+echo.
+echo :: FINALIZING
+echo.
 
 move ApiClient_build.cs.tmp ApiClient.cs
 rmdir /Q /S obj
