82989ad72ceac0a43b610baa4d378b3c548981d3
Pavel Djundik <xPaw@users.noreply.github.com>
2021-05-03 22:38:20 +0300

    Add a silly license file to archives

--- build.bat
+++ build.bat
@@ -19,7 +19,9 @@ del SteamTokenDumper.zip
 rmdir /Q /S obj
 rmdir /Q /S bin
 dotnet publish -c Release --runtime win-x64 --output bin/SteamTokenDumper /p:PublishSingleFile=true /p:PublishTrimmed=true
+copy release_license.txt bin\SteamTokenDumper\LICENSE.txt
 bash -c "unix2dos bin/SteamTokenDumper/SteamTokenDumper.config.ini"
+bash -c "unix2dos bin/SteamTokenDumper/LICENSE.txt"
 bash -c "cd bin && zip -9r ../SteamTokenDumper.zip SteamTokenDumper/"
 
 echo.
@@ -31,8 +33,10 @@ del SteamTokenDumper-linux.tar.gz
 rmdir /Q /S obj
 rmdir /Q /S bin
 dotnet publish -c Release --runtime linux-x64 --output bin/SteamTokenDumper /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true
+copy release_license.txt bin\SteamTokenDumper\LICENSE.txt
 bash -c "dos2unix bin/SteamTokenDumper/SteamTokenDumper.config.ini"
-bash -c "tar -I 'gzip -9' -cvf SteamTokenDumper-linux.tar.gz --owner=0 --group=0 -C bin/SteamTokenDumper/ SteamTokenDumper SteamTokenDumper.config.ini"
+bash -c "dos2unix bin/SteamTokenDumper/LICENSE.txt"
+bash -c "tar -I 'gzip -9' -cvf SteamTokenDumper-linux.tar.gz --owner=0 --group=0 -C bin/SteamTokenDumper/ SteamTokenDumper SteamTokenDumper.config.ini LICENSE.txt"
 
 echo.
 echo :: FINALIZING
