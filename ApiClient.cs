f9523e2af6327f550c4925f91066e3a860444563
Pavel Djundik <xPaw@users.noreply.github.com>
2021-11-26 13:59:42 +0200

    Use HTTP/3 by default

--- ApiClient.cs
+++ ApiClient.cs
@@ -24,6 +24,7 @@ internal class ApiClient : IDisposable
 #pragma warning disable CA5386 // Avoid hardcoding SecurityProtocolType value
         ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
 #pragma warning restore CA5386 // Avoid hardcoding SecurityProtocolType value
+        HttpClient.DefaultRequestVersion = HttpVersion.Version30;
         HttpClient.Timeout = TimeSpan.FromMinutes(10);
         HttpClient.DefaultRequestHeaders.Add("User-Agent", $"{nameof(SteamTokenDumper)} v{Version}");
     }
