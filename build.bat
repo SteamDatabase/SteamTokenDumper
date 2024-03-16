2cf8ff1fb0cb2fdb2123fd99bbee878ff317ae9d
Pavel Djundik <xPaw@users.noreply.github.com>
2024-03-16 22:14:54 +0200

    Copy full license file
    Fixes #2

--- build.bat
+++ build.bat
@@ -19,7 +19,7 @@ del SteamTokenDumper.zip
 rmdir /Q /S obj
 rmdir /Q /S bin
 dotnet publish -c Release --runtime win-x64 --self-contained true --output bin/SteamTokenDumper /p:PublishSingleFile=true /p:PublishTrimmed=true SteamTokenDumper.csproj
-copy release_license.txt bin\SteamTokenDumper\LICENSE.txt
+copy /b LICENSE+release_license.txt bin\SteamTokenDumper\LICENSE.txt
 bash -c "unix2dos bin/SteamTokenDumper/SteamTokenDumper.config.ini"
 bash -c "unix2dos bin/SteamTokenDumper/LICENSE.txt"
 bash -c "cd bin && zip -9rj ../SteamTokenDumper.zip SteamTokenDumper/"
@@ -33,7 +33,7 @@ del SteamTokenDumper-linux.tar.gz
 rmdir /Q /S obj
 rmdir /Q /S bin
 dotnet publish -c Release --runtime linux-x64 --self-contained true --output bin/SteamTokenDumper /p:PublishSingleFile=true /p:PublishTrimmed=true /p:IncludeNativeLibrariesForSelfExtract=true SteamTokenDumper.csproj
-copy release_license.txt bin\SteamTokenDumper\LICENSE.txt
+copy /b LICENSE+release_license.txt bin\SteamTokenDumper\LICENSE.txt
 bash -c "dos2unix bin/SteamTokenDumper/SteamTokenDumper.config.ini"
 bash -c "dos2unix bin/SteamTokenDumper/LICENSE.txt"
 bash -c "tar -I 'gzip -9' -cvf SteamTokenDumper-linux.tar.gz --owner=0 --group=0 -C bin/SteamTokenDumper/ SteamTokenDumper SteamTokenDumper.config.ini LICENSE.txt"
