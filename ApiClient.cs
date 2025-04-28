d91407caf5b87f86a2d772f4949c4f1af5ce719c
Pavel Djundik <xPaw@users.noreply.github.com>
2025-04-28 13:58:48 +0300

    Bump version

--- ApiClient.cs
+++ ApiClient.cs
@@ -16,7 +16,7 @@ namespace SteamTokenDumper;
 
 internal sealed class ApiClient : IDisposable
 {
-    public const uint Version = 1728691200; // 2024-10-12
+    public const uint Version = 1745798400; // 2025-04-28
 
     public const string Token = "@STEAMDB_BUILD_TOKEN@";
     private const string Endpoint = "https://tokendumper.steamdb.info";
